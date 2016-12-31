3e5d21b850e3afa3df29c5d9254b29c970df3378
Nick <nick@smartgreen.co.il>
2016-12-31 20:41:25 +0200

    added a rest API, might want to use volley in the main activity.

---
+++ app/src/main/java/org/iarc/nick/hamradioiarc/restAPI.java
@@ -0,0 +1,36 @@
+package org.iarc.nick.hamradioiarc;
+
+/**
+ * Created by all on 30.12.2016.
+ */
+public class restAPI {
+    private String answer1 = "Its the journey of bullshit";
+    private String answer2 = "private String answer3 is probably right ";
+    private String answer3 = "42";
+    private String answer4 = "I don't know";
+    private String question = "The answer to life universe and everything";
+
+    public restAPI() {
+    }
+
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+}
